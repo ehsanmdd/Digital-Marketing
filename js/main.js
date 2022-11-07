086ec9d459cc6a519cfe2e994ab362dcbba5fca5
Ehsan Motamedi <ehsan.mtd@yahoo.com>
2022-11-07 23:48:32 +0330

    End of Article section

--- js/main.js
+++ js/main.js
@@ -14,4 +14,13 @@ window.onload = function digital_fn (){
         collapse.classList.toggle("active")
 
     })
+
+    //Masonry js
+
+    let grid = document.querySelector("#site-main .recent-work-area .images-flex")
+    let msnry = new Masonry(grid, {
+        itemSelector: ".flex-item",
+        gutter :100,
+        fitwidth : true
+    })
 }
